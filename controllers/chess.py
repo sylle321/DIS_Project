8f1cbc6082d2088eed239ba87eae57f0ae8a88a0
sylle321 <syllevest@hotmail.com>
2026-06-03 12:56:39 +0200

    Update

--- controllers/chess.py
+++ controllers/chess.py
@@ -1,36 +1,43 @@
 from flask import Blueprint, render_template, request
-from models.chess_stats import get_best_moves, get_position_key_from_fen
+from models.chess_stats import get_best_moves, get_position_key_from_moves
 
 bp = Blueprint('chess', __name__, url_prefix='/')
+
 
 @bp.route('/chess', methods=['GET', 'POST'])
 def chess():
     moves = []
-    fen = ""
+    moves_text = ""
     my_rating = ""
     opponent_rating = ""
     my_color = "white"
+    error = ""
 
     if request.method == 'POST':
-        fen = request.form['fen']
+        moves_text = request.form['moves_text']
         my_rating = int(request.form['my_rating'])
         opponent_rating = int(request.form['opponent_rating'])
         my_color = request.form['my_color']
 
-        position_key = get_position_key_from_fen(fen)
+        try:
+            position_key = get_position_key_from_moves(moves_text)
 
-        moves = get_best_moves(
-            position_key,
-            my_rating,
-            opponent_rating,
-            my_color
-        )
+            moves = get_best_moves(
+                position_key,
+                my_rating,
+                opponent_rating,
+                my_color
+            )
+
+        except ValueError:
+            error = "Ugyldigt træk. Prøv fx: e4, e4 e5 eller e4 e5 Nf3"
 
     return render_template(
         'chess.html',
         moves=moves,
-        fen=fen,
+        moves_text=moves_text,
         my_rating=my_rating,
         opponent_rating=opponent_rating,
-        my_color=my_color
+        my_color=my_color,
+        error=error
     )
